020ad8bcf082e225a06dc211fb3bc99bb8226c31
alexwinx <winxalex@gmail.com>
2020-04-23 15:20:41 +0200

    formating data and filling the Cards

--- my-app/app/pages/components/Card.js
+++ my-app/app/pages/components/Card.js
@@ -15,7 +15,7 @@ export default function Card({ subtitle, titleLeftBadge, title, titleRightBadge,
                 <h3 className={`${styles["card-subtitle"]} text-center text-uppercase font-weight-bold`}>{subtitle}</h3>
                 <div className={`text-center`}>
                     <span className={styles.titleLeftBadge}>{titleLeftBadge}</span>&nbsp;
-                        <span className={styles["card-title"]}>ggg{title}</span>
+                        <span className={styles["card-title"]}>{title}</span>
                     <span className={styles.titleRightBadge}>{titleRightBadge}</span>
                 </div>
                 <h5 className={`styles.cardTitleDescripiton text-center`}>{titleDescription}</h5>
@@ -25,16 +25,18 @@ export default function Card({ subtitle, titleLeftBadge, title, titleRightBadge,
             <p className={`${styles["card-text"]} text-center`}>{text}</p>
             <ul className={styles.cardList}>
 
-                <li className={styles.cardListItem}>
-                    <svg className={styles.cardListBullet} width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M8.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L10.793 8 8.146 5.354a.5.5 0 010-.708z" clipRule="evenodd" />
-                        <path fillRule="evenodd" d="M4 8a.5.5 0 01.5-.5H11a.5.5 0 010 1H4.5A.5.5 0 014 8z" clipRule="evenodd" />
-                    </svg><span style={{ marginLeft: "5px" }} > Mile</span></li>
-                <li className={styles.cardListItem}>
-                    <svg className={styles.cardListBullet} width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M8.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L10.793 8 8.146 5.354a.5.5 0 010-.708z" clipRule="evenodd" />
-                        <path fillRule="evenodd" d="M4 8a.5.5 0 01.5-.5H11a.5.5 0 010 1H4.5A.5.5 0 014 8z" clipRule="evenodd" />
-                    </svg><span style={{ marginLeft: "5px" }} > Mile</span></li>
+                {
+                    options && options.map((el, i) =>
+
+                        <li key={i} className={styles.cardListItem}>
+                            <svg className={styles.cardListBullet} width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M8.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L10.793 8 8.146 5.354a.5.5 0 010-.708z" clipRule="evenodd" />
+                                <path fillRule="evenodd" d="M4 8a.5.5 0 01.5-.5H11a.5.5 0 010 1H4.5A.5.5 0 014 8z" clipRule="evenodd" />
+                            </svg><span style={{ marginLeft: "5px" }} > {el}</span></li>
+
+                    )
+                }
+
 
 
             </ul>
@@ -43,6 +45,8 @@ export default function Card({ subtitle, titleLeftBadge, title, titleRightBadge,
             <div className={`text-center`}>
                 <a href="#" className={`${styles.btn} ${styles["btn-primary"]} text-center`} onClick={onSelect}>Select</a>
             </div>
+            <br />
+            <div>&nbsp;</div>
         </div>
 
     )
--- my-app/app/pages/pricing/[currency].js
+++ my-app/app/pages/pricing/[currency].js
@@ -3,21 +3,107 @@ import fetch from 'node-fetch'
 import Layout from '../components/Layout';
 import CardGroup from '../components/CardGroup';
 import Card from '../components/card';
+import plansConfig from "./plans.config.json";
+import { useState } from 'react';
 
-const Plans = ({ stars }) => {
+
+
+const Plans = ({ plans }) => {
+
+
     const router = useRouter()
+    const [state, SetState] = useState({})
     const { currency } = router.query;
-    console.log(router.query);
+
+    console.log(plansConfig);
+
+    const handleCurrencyChange = (e) => {
+        e.preventDefault();
+        router.push("/pricing/" + e.target.value);
+    }
+
+
+    const handlePeriodChange = (e) => {
+
+    }
+
+
+    function bytesToSize(bytes) {
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes == 0) return '0 Byte';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+    }
+
+    function plansToLeftBadge({ Pricing }) {
+        console.log(Pricing);
+    }
+    function plansToLeftBadge({ Pricing }) {
+        console.log(Pricing);
+    }
+    function plansToTitle({ Pricing }) {
+        console.log(Pricing);
+    }
+
+    function plansToOptions({ MaxDomains, MaxAddresses, MaxSpace, MaxMembers }) {
+        const options = [];
+
+        if (MaxMembers > 0) {
+            options.push(`${MaxMembers} user`);
+        }
+
+        if (MaxSpace > 0) {
+            options.push(`${bytesToSize(MaxSpace)} *`);
+        }
+
+        if (MaxAddresses > 0) {
+            options.push(`${MaxAddresses} addresses *`);
+        }
+
+        if (MaxDomains > 0) {
+            options.push(`Supports ${MaxDomains} domain *`);
+        } else {
+            options.push(`No domain support`);
+        }
+
+
+
+        return options;
+    }
 
 
     return (
         <div>
             <Layout>
+                <div style={{ float: "right" }}>
+                    <select onChange={handlePeriodChange}>
+                        <option>Monthly</option>
+                        <option>Annually</option>
+                        <option>2 years</option>
+                    </select>
+
+                    <select onChange={handleCurrencyChange}>
+                        <option>EUR</option>
+                        <option>USD</option>
+                        <option>CHF</option>
+                    </select>
+                </div>
                 <CardGroup>
-                    <Card></Card>
+                    {
+                        plans.map((plan, index) =>
+
+                            <Card key={index}
+                                title={plansToTitle(plan)}
+                                subtitle={plan.Name}
+                                titleLeftBadge={plansToLeftBadge(plan)}
+                                options={plansToOptions(plan)}></Card>
+
+                        )
+                    }
+
                 </CardGroup>
             </Layout>
-            <p>Post: {currency} {stars}</p>
+
         </div>
     )
 }
@@ -61,7 +147,8 @@ export async function getStaticProps({ params }) {
     //return { stars: json.stargazers_count }
     console.log(json);
     // return { props: { stars: json.Plans[0].Name } }
-    return { props: { stars: JSON.stringify(json) } }
+    //return { props: { stars: JSON.stringify(json) } }
+    return { props: { plans: json.Plans } }
     //return { props: { stars: params.i } }
 };
 
